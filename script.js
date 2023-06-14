2b838b1674687a9166512363417279340b46d1fd
Barzat <ivy.mike555@gmail.com>
2023-06-14 15:55:19 +0600

    new file: index.html new file: style.css new file: script.css

---
+++ script.js
@@ -0,0 +1,17 @@
+const div = document.querySelector(".content");
+
+const promise = fetch("https://www.omdbapi.com/?apikey=b6003d8a&s=All");
+
+promise
+  .then((response) => response.json())
+  .then(({ Search }) => {
+    for (let i = 0; i < Search.length; i++) {
+      console.log(Search[i].Title, "dfdf");
+      div.innerHTML += `
+        <div class="container">
+          <img src="${Search[i].Poster}" alt="" />
+          <h5>${Search[i].Title}</h5>
+          <span>${Search[i].Year}</span>
+        </div>`;
+    }
+  });
